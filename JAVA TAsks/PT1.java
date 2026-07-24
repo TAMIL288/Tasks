bde61114966012c395a81ec075586df9545a16cf
TAMIL288 <tamilarasu232006@gmail.com>
2026-07-24 16:21:59 +0530

    add

---
+++ JAVA TAsks/PT1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class PT1 {
+    public static void main(String args[]){
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            System.out.println(n);
+        }
+
+    }
+    
+}
