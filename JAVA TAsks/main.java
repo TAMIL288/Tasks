4c125bb4d5ddb868b92a8a356b751a8708a47d6b
TAMIL288 <tamilarasu232006@gmail.com>
2026-07-24 13:56:48 +0530

    add

--- JAVA TAsks/main.java
+++ JAVA TAsks/main.java
@@ -1,15 +1,16 @@
-import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
 
-        Scanner scan=new Scanner(System.in);
+        int[] arr = {15, 60, 25, 90, 40};
 
-        int n= scan.nextInt();
+        int max = arr[0];
 
-        int [] arr=new int[n];
-
-        for(int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]= scan.nextInt());
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
         }
+
+        System.out.println("Largest = " + max);
     }
 }
